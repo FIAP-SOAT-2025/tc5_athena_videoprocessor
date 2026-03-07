c7345932248e1646671e9980a8b2272d7024033a
tlnob <thais.nobre7@gmail.com>
2026-03-07 12:30:45 -0300

    add health endpoint; add actions

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { VideoModule } from './video/video.module';
 import { StorageModule } from './storage/storage.module';
 import { PrometheusModule } from '@willsoto/nestjs-prometheus';
+import { HealthModule } from './health/health.module';
 
 @Module({
   imports: [
     PrometheusModule.register(),
     VideoModule,
     StorageModule,
+    HealthModule,
   ],
   controllers: [],
   providers: [],
